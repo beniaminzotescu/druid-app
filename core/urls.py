f21c4fc0c4767c50407abbfa9518753647486252
BeniaminZotescu <beniamin.zotescu@druidai.com>
2023-08-29 14:54:58 +0300

    static files+ druid chat

--- core/urls.py
+++ core/urls.py
@@ -1,16 +1,20 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from druid_app import settings
 from .forms import LoginForm
 from . import views
-
 
 app_name = 'core'
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm),
+         name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('thank-you/', views.thank_you, name='thank_you'),  # Add this line
 
 ]
 
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
