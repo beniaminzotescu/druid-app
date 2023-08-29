7c9cc795014e4adecb1c8564018960318039d775
BeniaminZotescu <beniamin.zotescu@druidai.com>
2023-08-29 15:44:45 +0300

    static files+ druid chat

--- druid_app/settings.py
+++ druid_app/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     ('core', os.path.join(BASE_DIR, 'core', 'static')),
