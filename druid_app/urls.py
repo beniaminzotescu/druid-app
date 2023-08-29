88634e43a751ac8be5b90ac2985a0f3ce367ab4d
BeniaminZotescu <beniamin.zotescu@druidai.com>
2023-08-29 15:28:43 +0300

    static files+ druid chat

--- druid_app/urls.py
+++ druid_app/urls.py
@@ -7,7 +7,4 @@ from druid_app import settings
 urlpatterns = [
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
