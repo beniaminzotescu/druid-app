3ebc5de18ec678ad8e99599f53e85afd171063b8
BeniaminZotescu <beniamin.zotescu@druidai.com>
2023-08-31 09:42:32 +0300

    pages

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,12 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('enterprise/', views.enterprise, name='enterprise'),
+    path('industrial/', views.industrial, name='industrial'),
+    path('platinum/', views.platinum, name='platinum'),
+    path('gold/', views.gold, name='gold'),
+    path('silver/', views.silver, name='silver'),
+    path('contact/', views.contact, name='contact'),
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm),
          name='login'),
     path('logout/', views.logout_view, name='logout'),
--- core/views.py
+++ core/views.py
@@ -10,6 +10,36 @@ def index(request):
     return render(request, 'core/index.html')
 
 
+@login_required
+def enterprise(request):
+    return render(request, 'core/enterprise.html')
+
+
+@login_required
+def industrial(request):
+    return render(request, 'core/industrial.html')
+
+
+@login_required
+def platinum(request):
+    return render(request, 'core/platinum.html')
+
+
+@login_required
+def gold(request):
+    return render(request, 'core/gold.html')
+
+
+@login_required
+def silver(request):
+    return render(request, 'core/silver.html')
+
+
+@login_required
+def contact(request):
+    return render(request, 'core/contact.html')
+
+
 def logout_view(request):
     response = redirect('/thank-you/')  # Redirect to thank you page
     response.set_cookie('logged_out', 'true', max_age=settings.LOG_OUT_COOKIE_DURATION)
