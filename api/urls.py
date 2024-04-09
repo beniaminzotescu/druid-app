7a5a855c271a38a45d29fa1cfd79ad6f57ac42a0
BeniaminZotescu <beniamin.zotescu@druidai.com>
2024-04-09 11:53:03 +0300

    style

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('test-json/', TestJsonAPIView.as_view(), name='test-json'),
     path('test-json/<str:url>/', TestJsonAPIView.as_view(), name='test-json-detail'),
 ]
+pass
 
