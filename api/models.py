3d383828520405047913b2022a9a9c8f934df549
BeniaminZotescu <beniamin.zotescu@druidai.com>
2024-04-09 11:04:48 +0300

    style

--- api/models.py
+++ api/models.py
@@ -1,6 +1,6 @@
+# myapp/models.py
 from django.db import models
 
 class TestData(models.Model):
+    url = models.CharField(max_length=10, unique=True)
     json_data = models.JSONField()
-    url = models.CharField(max_length=10, unique=True)  # Adjust the length of the URL as needed
-
--- api/serializers.py
+++ api/serializers.py
@@ -1,16 +1,11 @@
+# myapp/serializers.py
 from rest_framework import serializers
 from .models import TestData
 
+class TestDataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TestData
+        fields = ['url', 'json_data']
 
-class TestDataSerializer(serializers.Serializer):
-    json_data = serializers.JSONField()
-
-    def create(self, validated_data):
-        # If you want to create a new row every time
-        return TestData.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        # If you want to overwrite existing data
-        instance.json_data = validated_data.get('json_data', instance.json_data)
-        instance.save()
-        return instance
+    def to_representation(self, instance):
+        return instance.json_data
